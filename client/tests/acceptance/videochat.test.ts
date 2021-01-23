da67fbcf3dcc403bb5265df4b331f69f339d61f9
Enrico Graziani <enrico.graziani@cazoo.co.uk>
2021-01-23 12:51:28 +0000

    WIP boh

---
+++ client/tests/acceptance/videochat.test.ts
@@ -0,0 +1,11 @@
+// videochat app
+    // when launched
+        // open the camera
+        // start 3D world
+            // start the network layer
+
+describe('videochat app', () => {
+    describe('when launched', () => {
+           it('should try open the camera', () => {})
+    });
+});
